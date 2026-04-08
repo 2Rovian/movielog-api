3dfc2a1b26a64a94f75978a65ff386fc6d47a8e8
Ian Gurgel <iangurgel25@hotmail.com>
2026-04-08 16:04:07 -0300

    first commit

---
+++ src/main/java/com/movielog/movielog_api/MovielogApiApplication.java
@@ -0,0 +1,13 @@
+package com.movielog.movielog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovielogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovielogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movielog/movielog_api/MovielogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movielog.movielog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
