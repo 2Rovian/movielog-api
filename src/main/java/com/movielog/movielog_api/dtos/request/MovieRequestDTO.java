e26091844dfa139121163752319dcb8596f9a598
Ian Gurgel <iangurgel25@hotmail.com>
2026-04-08 16:19:53 -0300

    feat: MovieRequestDTO and MovieResponseDTO

---
+++ src/main/java/com/movielog/movielog_api/dtos/request/MovieRequestDTO.java
@@ -0,0 +1,4 @@
+package com.movielog.movielog_api.dtos.request;
+
+public record MovieRequestDTO(String title, Double imdb) {
+}
---
+++ src/main/java/com/movielog/movielog_api/dtos/response/MovieResponseDTO.java
@@ -0,0 +1,4 @@
+package com.movielog.movielog_api.dtos.response;
+
+public record MovieResponseDTO(Long id, String title, Double imdb) {
+}
